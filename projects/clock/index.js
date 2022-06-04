ec1873633277a6da978c8df44e61a842f5f7580d
Sahandgha <sahand.gha@gmail.com>
2022-06-05 07:51:55 +1000

    add clock project

---
+++ projects/clock/index.js
@@ -0,0 +1,21 @@
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+
+function setDate() {
+  const now = new Date();
+
+  const getSecond = now.getSeconds();
+  const getMinute = now.getMinutes();
+  const getHour = now.getHours();
+
+  const secondDegree = (getSecond / 60) * 360;
+  const minuteDegree = (getMinute / 60) * 360;
+  const hourDegree = (getHour / 12) * 360;
+
+  second.style.transform = `rotate(${secondDegree}deg)`;
+  minute.style.transform = `rotate(${minuteDegree}deg)`;
+  hour.style.transform = `rotate(${hourDegree}deg)`;
+}
+
+setInterval(setDate, 1000);
