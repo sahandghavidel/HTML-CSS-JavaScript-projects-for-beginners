1b8a8f0f8ec7d5cb881443fc6063c81132fda862
Sahandgha <sahand.gha@gmail.com>
2022-06-13 08:34:16 +1000

    fix month calender project

---
+++ projects/month-calender/index.js
@@ -0,0 +1,41 @@
+const monthEl = document.querySelector(".date h1");
+const fullDateEl = document.querySelector(".date p");
+const daysEl = document.querySelector(".days");
+
+const monthInx = new Date().getMonth();
+const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
+const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1;
+
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+monthEl.innerText = months[monthInx];
+fullDateEl.innerText = new Date().toDateString();
+
+let days = "";
+
+for (let i = firstDay; i > 0; i--) {
+  days += `<div class="empty"></div>`;
+}
+for (let i = 1; i <= lastDay; i++) {
+  if (i === new Date().getDate()) {
+    days += `<div class="today">${i}</div>`;
+  } else {
+    days += `<div>${i}</div>`;
+  }
+}
+
+daysEl.innerHTML = days;
