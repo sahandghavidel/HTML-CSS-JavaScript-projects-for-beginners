266dca8e5261901995b29ae3e0a17ee454596b4a
Sahandgha <sahand.gha@gmail.com>
2022-11-28 15:17:23 +0800

    update currency converter project

--- projects/currency-converter/index.js
+++ projects/currency-converter/index.js
@@ -1,51 +1,27 @@
-const exchange_rate = document.getElementById('exchange-rate');
-const curr_first = document.getElementById('curr-first');
-const curr_second = document.getElementById('curr-second');
-const worth_first = document.getElementById('worth-first');
-const worth_second = document.getElementById('worth-second');
+const exchangeRateEl = document.getElementById("exchange-rate");
+const currencyFirstEl = document.getElementById("currency-first");
+const currencySecondEl = document.getElementById("currency-second");
+const worthFirstEl = document.getElementById("worth-first");
+const worthSecondEl = document.getElementById("worth-second");
 
 function convert() {
-  const currency_first = curr_first.value;
-  const currency_second = curr_second.value;
-//using API for conversion of currency units
-  fetch(`https://v6.exchangerate-api.com/v6/16947c81da979880bacde4f5/latest/${currency_first}`)
+  const currencyFirstValue = currencyFirstEl.value;
+  const currencySecondValue = currencySecondEl.value;
+  //using API for conversion of currency units
+  fetch(
+    `https://v6.exchangerate-api.com/v6/5f9d1c87f7250159c9c9b17d/latest/${currencyFirstValue}`
+  )
     .then((res) => res.json())
     .then((data) => {
-      
-      const rate = data.conversion_rates[currency_second];
-      exchange_rate.innerText = `1 ${currency_first} = ${rate} ${currency_second}`;
+      const rate = data.conversion_rates[currencySecondValue];
+      exchangeRateEl.innerText = `1 ${currencyFirstValue} = ${rate} ${currencySecondValue}`;
 
-      worth_second.value = (worth_first.value * rate).toFixed(5);
+      worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2);
     });
 }
 //some javascript event listeners
-curr_first.addEventListener('change', convert);
-worth_first.addEventListener('input', convert);
-curr_second.addEventListener('change', convert);
-worth_second.addEventListener('input', convert);
+currencyFirstEl.addEventListener("change", convert);
+worthFirstEl.addEventListener("input", convert);
+currencySecondEl.addEventListener("change", convert);
+worthSecondEl.addEventListener("input", convert);
 convert();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
