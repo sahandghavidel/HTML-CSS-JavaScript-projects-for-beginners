8a5ba99f6afc434532c833ccba589f0849d14ab7
Sahandgha <sahand.gha@gmail.com>
2022-12-04 09:52:06 +0700

    Update anime pics generator

--- projects/amine-pics-generator/index.js
+++ projects/amine-pics-generator/index.js
@@ -1,26 +1,25 @@
 const btnEl = document.getElementById("btn");
-const animeBoxEl = document.querySelector(".anime-container");
+const animeContainerEl = document.querySelector(".anime-container");
 const animeImgEl = document.querySelector(".anime-img");
-const animeNameEl = document.querySelector(".anime-name");
+const amineNameEl = document.querySelector(".anime-name");
 
 btnEl.addEventListener("click", async function () {
   try {
     btnEl.disabled = true;
     btnEl.innerText = "Loading...";
-    animeNameEl.innerText = "Updating...";
+    amineNameEl.innerText = "Updating...";
     animeImgEl.src = "spinner.svg";
     const response = await fetch("https://api.catboys.com/img");
     const data = await response.json();
-    animeImgEl.src = data.url;
-
-    animeBoxEl.style.display = "block";
-    animeNameEl.innerText = data.artist;
     btnEl.disabled = false;
     btnEl.innerText = "Get Anime";
+    animeContainerEl.style.display = "block";
+    animeImgEl.src = data.url;
+    amineNameEl.innerText = data.artist;
   } catch (error) {
     console.log(error);
     btnEl.disabled = false;
     btnEl.innerText = "Get Anime";
-    animeNameEl.innerText = "An error happened, try again";
+    amineNameEl.innerText = "An error happened, please try again";
   }
 });
--- projects/currency-converter/index.js
+++ projects/currency-converter/index.js
@@ -1,27 +1,33 @@
-const exchangeRateEl = document.getElementById("exchange-rate");
 const currencyFirstEl = document.getElementById("currency-first");
-const currencySecondEl = document.getElementById("currency-second");
+
 const worthFirstEl = document.getElementById("worth-first");
+
+const currencySecondEl = document.getElementById("currency-second");
+
 const worthSecondEl = document.getElementById("worth-second");
 
-function convert() {
-  const currencyFirstValue = currencyFirstEl.value;
-  const currencySecondValue = currencySecondEl.value;
-  //using API for conversion of currency units
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+updateRate()
+
+function updateRate() {
   fetch(
-    `https://v6.exchangerate-api.com/v6/5f9d1c87f7250159c9c9b17d/latest/${currencyFirstValue}`
+    `https://v6.exchangerate-api.com/v6/5f9d1c87f7250159c9c9b17d/latest/${currencyFirstEl.value}`
   )
     .then((res) => res.json())
     .then((data) => {
-      const rate = data.conversion_rates[currencySecondValue];
-      exchangeRateEl.innerText = `1 ${currencyFirstValue} = ${rate} ${currencySecondValue}`;
+      const rate = data.conversion_rates[currencySecondEl.value];
+      console.log(rate);
+      exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${
+        rate + " " + currencySecondEl.value
+      }`;
 
-      worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2);
+      worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2)
     });
 }
-//some javascript event listeners
-currencyFirstEl.addEventListener("change", convert);
-worthFirstEl.addEventListener("input", convert);
-currencySecondEl.addEventListener("change", convert);
-worthSecondEl.addEventListener("input", convert);
-convert();
+
+currencyFirstEl.addEventListener("change", updateRate);
+
+currencySecondEl.addEventListener("change", updateRate);
+
+worthFirstEl.addEventListener("input", updateRate);
