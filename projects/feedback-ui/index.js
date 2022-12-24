420c6ab233216d2a429716d6e46abef08fb7df8a
Sahandgha <sahand.gha@gmail.com>
2022-12-24 16:12:39 +0700

    update the feedback ui project

--- projects/feedback-ui/index.js
+++ projects/feedback-ui/index.js
@@ -1,27 +1,29 @@
 const ratingEls = document.querySelectorAll(".rating");
-const ratingsContainerEl = document.getElementById("ratings-container");
 const btnEl = document.getElementById("btn");
+
 const containerEl = document.getElementById("container");
+
 let selectedRating = "";
 
 ratingEls.forEach((ratingEl) => {
-  ratingEl.addEventListener("click", (e) => {
+  ratingEl.addEventListener("click", (event) => {
     removeActive();
-    selectedRating = e.target.innerText || e.target.parentNode.innerText;
-    e.target.parentNode.classList.add("active");
-    e.target.classList.add("active");
+    selectedRating =
+      event.target.innerText || event.target.parentNode.innerText;
+    event.target.classList.add("active");
+    event.target.parentNode.classList.add("active");
   });
 });
 
 btnEl.addEventListener("click", () => {
   if (selectedRating !== "") {
     containerEl.innerHTML = `
-        <strong>Thank You!</strong>
+        <strong>Thank you!</strong>
         <br>
         <br>
-        <strong>Feedback : ${selectedRating}</strong>
+        <strong>Feedback: ${selectedRating}</strong>
         <p>We'll use your feedback to improve our customer support.</p>
-    `;
+        `;
   }
 });
 
