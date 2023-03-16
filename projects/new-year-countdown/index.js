ce561ee048ed7f436a0cd0110fdae0e4ab708232
Sahandgha <sahand.gha@gmail.com>
2023-03-16 10:53:15 +0700

    update and clean files

--- projects/new-year-countdown/index.js
+++ projects/new-year-countdown/index.js
@@ -3,7 +3,7 @@ const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minute");
 const secondEl = document.getElementById("second");
 
-const newYearTime = new Date("Jan 1, 2022 00:00:00").getTime();
+const newYearTime = new Date("Jan 1, 2024 00:00:00").getTime();
 
 updateCountdown();
 
