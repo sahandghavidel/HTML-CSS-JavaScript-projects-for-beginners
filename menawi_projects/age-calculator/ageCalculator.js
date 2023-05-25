8bd7956aee28a7c8ff355727bf7b422933741739
Omar Elmenawi <omar.menawi@gmail.com>
2023-05-25 13:04:40 -0400

    fish say when it swam into a wall? Damn

---
+++ menawi_projects/age-calculator/ageCalculator.js
@@ -0,0 +1,55 @@
+
+// ------------------------
+
+// ⭐️ My personal library ⭐️
+
+// create element
+const createElement = (elementType) => { return document.createElement(elementType) }
+// append element to document
+const appendElement = (element) => { document.body.appendChild(element); }
+// change the inner text of element
+const innerText = (element, value) => { element.innerText = value }
+// get element by id
+const getid = (elementid) => { return document.getElementById(elementid) }
+// function to show my results in the document
+function showMyResult(param) {
+    let elementToAppend = createElement("p");
+    appendElement(elementToAppend);
+    innerText(elementToAppend, param);
+}
+// run function every x seconds
+const runInterval = (actions, timer) => setInterval(
+    () => { actions() }, timer
+)
+
+function runInterval(actions, timer) {
+
+    let result = setInterval( () => {
+        actions
+    }, timer);
+
+    return result;
+}
+
+function doSomething() {
+    console.log("do something");
+}
+
+
+
+
+
+
+// runInterval(showMyResult(), 1000)
+
+
+// ------------------------
+
+// 🗓️ Age Calculator 🗓️ 
+
+// CONSTANTS
+
+const button = getid("btn");
+button.addEventListener("click", () => { showMyResult("button works!") });
+
+
