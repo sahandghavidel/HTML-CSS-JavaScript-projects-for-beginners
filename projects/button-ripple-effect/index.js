4ad3011290064abd3dfc2a67ab0d9d8dac98430f
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2023-07-12 00:18:35 +0530

    Chnages the Repel Effect #6

--- projects/button-ripple-effect/index.js
+++ projects/button-ripple-effect/index.js
@@ -1,9 +1,32 @@
-const btnEl = document.querySelector(".btn");
+// const btnEl = document.querySelector(".btn");
 
-btnEl.addEventListener("mouseover", (event) => {
-  const x = event.pageX - btnEl.offsetLeft;
-  const y = event.pageY - btnEl.offsetTop;
+// btnEl.addEventListener("mouseover", (event) => {
+//   const x = event.pageX - btnEl.offsetLeft;
+//   const y = event.pageY - btnEl.offsetTop;
 
-  btnEl.style.setProperty("--xPos", x + "px");
-  btnEl.style.setProperty("--yPos", y + "px");
-});
+//   btnEl.style.setProperty("--xPos", x + "px");
+//   btnEl.style.setProperty("--yPos", y + "px");
+// });
+const buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
