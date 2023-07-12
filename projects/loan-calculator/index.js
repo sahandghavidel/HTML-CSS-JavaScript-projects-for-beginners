0a26c623ae15687d0069b5ab4069e525d6ffeb2c
Akshat Verma <112319520+Akshat162001@users.noreply.github.com>
2023-07-13 00:03:06 +0530

    made the working laon calculator

--- projects/loan-calculator/index.js
+++ projects/loan-calculator/index.js
@@ -1,15 +1,21 @@
 function calculateLoan() {
-  loanAmountValue = document.getElementById("loan-amount").value;
+  event.preventDefault();
 
-  interestRateValue = document.getElementById("interest-rate").value;
+   var loanAmountValue = document.getElementById("loan-amount").value;
 
-  MonthsToPayValue = document.getElementById("months-to-pay").value;
+   var interestRateValue = document.getElementById("interest-rate").value;
 
-  interest = (loanAmountValue * (interestRateValue * 0.01)) / MonthsToPayValue;
+  var MonthsToPayValue = document.getElementById("months-to-pay").value;
+
+ var interest = (loanAmountValue * (interestRateValue * 0.01)) / MonthsToPayValue;
 
   monthlyPayment = (loanAmountValue / MonthsToPayValue + interest).toFixed(2);
 
-  document.getElementById(
-    "payment"
-  ).innerHTML = `Monthly Payment: ${monthlyPayment}`;
+ 
+  document.getElementById("payment").innerHTML="Total Loan to be paid is Ruppees " + monthlyPayment
+  document.getElementById("loan-amount").value = "";
+  document.getElementById("interest-rate").value = "";
+  document.getElementById("months-to-pay").value = "";
+
+   
 }
